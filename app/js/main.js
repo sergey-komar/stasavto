14f955e0247fd316e13e4330934d1f67f69683c7
sergey-komar <kursev81@yandex.ru>
2023-02-15 17:30:54 +0300

    desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev slick-prev-img"></button>',
+        nextArrow : '<button type="button" class="slick-next slick-next-img"></button>'
+    })
 })
